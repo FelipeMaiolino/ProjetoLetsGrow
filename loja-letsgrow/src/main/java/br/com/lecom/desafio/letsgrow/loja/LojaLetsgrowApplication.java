d4b831b2271e2d9fd5d481a58fcbd8207476e7c6
FelipeMaiolino <43590547+FelipeMaiolino@users.noreply.github.com>
2021-11-09 10:17:09 -0300

    Base
    Base spring

---
+++ loja-letsgrow/src/main/java/br/com/lecom/desafio/letsgrow/loja/LojaLetsgrowApplication.java
@@ -0,0 +1,13 @@
+package br.com.lecom.desafio.letsgrow.loja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaLetsgrowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaLetsgrowApplication.class, args);
+	}
+
+}
---
+++ loja-letsgrow/src/test/java/br/com/lecom/desafio/letsgrow/loja/LojaLetsgrowApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lecom.desafio.letsgrow.loja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaLetsgrowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
